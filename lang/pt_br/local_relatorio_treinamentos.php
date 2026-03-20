f17b6bd79b5c30475366e5035af5c9a7b226bdb1
Mauricio <mauricioss777@gmail.com>
2026-03-19 23:11:34 -0300

    feat(lang): separar strings en/pt_br e atualizar versão para 2026031900

---
+++ lang/pt_br/local_relatorio_treinamentos.php
@@ -0,0 +1,18 @@
+<?php
+$string['pluginname']                      = 'Relatório de Treinamentos';
+$string['taskname']                        = 'Atualizar Relatório de Treinamentos';
+$string['relatorio_treinamentos:view']     = 'Ver Relatório de Treinamentos';
+$string['noaccess']                        = 'Você não tem permissão para acessar o Relatório de Treinamentos.';
+$string['setting_colunas_visiveis']        = 'Colunas visíveis por padrão';
+$string['setting_colunas_visiveis_desc']   = 'Selecione quais colunas aparecem visíveis quando o usuário abre o relatório pela primeira vez (sem preferência salva no navegador).';
+$string['setting_filtros_visiveis']        = 'Filtros disponíveis no relatório';
+$string['setting_filtros_visiveis_desc']   = 'Selecione quais campos de filtro serão exibidos no painel de filtros do relatório. Apenas os campos selecionados aparecerão para o usuário.';
+$string['setting_agrupamentos_zip']        = 'Agrupamentos disponíveis no download ZIP';
+$string['setting_agrupamentos_zip_desc']   = 'Selecione quais campos estarão disponíveis como opções de agrupamento no download ZIP do relatório.';
+$string['setting_filtros_visiveis_gestor']      = 'Filtros disponíveis para gestores';
+$string['setting_filtros_visiveis_gestor_desc'] = 'Selecione quais campos de filtro serão exibidos para usuários identificados como gestores. Se não configurado, usa os mesmos filtros do setting geral.';
+$string['setting_downloads_visiveis']             = 'Downloads disponíveis (Admins/Managers)';
+$string['setting_downloads_visiveis_desc']        = 'Selecione quais opções de download estarão disponíveis na tela do relatório para administradores e managers.';
+$string['setting_downloads_visiveis_gestor']      = 'Downloads disponíveis (Gestores)';
+$string['setting_downloads_visiveis_gestor_desc'] = 'Selecione quais opções de download estarão disponíveis na tela do relatório para usuários identificados como gestores.';
+$string['setting_python_warning'] = 'Python 3 não está configurado neste servidor. A exportação XLSX ficará indisponível até que <code>pathtopython</code> seja definido em <a href="/admin/settings.php?section=systempaths">Administração > Servidor > Caminhos do sistema</a> e o Python esteja instalado com as dependências <code>pandas</code> e <code>openpyxl</code>.';
--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_relatorio_treinamentos';
-$plugin->version   = 2026031100;
+$plugin->version   = 2026031900;
 $plugin->requires  = 2022041900; // Moodle 4.0+
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.1.0';
